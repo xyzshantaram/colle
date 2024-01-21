3eb00e5883f21abbe9f19ba07594a69bc78643ab
Siddharth Singh <me@shantaram.xyz>
2024-01-21 15:45:43 +0530

    add css for alert lib

--- public/deps.js
+++ public/deps.js
@@ -1,4 +1,4 @@
 import cf from "https://esm.sh/campfire.js@4.0.0-alpha2";
-import { confirm, message, input } from "https://esm.sh/cf-alert@0.4.1";
+import { confirm, message, input, fatal } from "https://esm.sh/cf-alert@0.4.1";
 
-export { cf, confirm, message, input };
+export { cf, confirm, message, input, fatal };
