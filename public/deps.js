477c7787cdfe9e1ed13997099f2dbad515227a63
Siddharth Singh <me@shantaram.xyz>
2024-01-22 02:18:47 +0530

    move getName into deps

--- public/deps.js
+++ public/deps.js
@@ -1,4 +1,11 @@
 import cf from "https://esm.sh/campfire.js@4.0.0-alpha2";
 import { confirm, message, input, fatal } from "https://esm.sh/cf-alert@0.4.1";
 
-export { cf, confirm, message, input, fatal };
+const getName = (file, quoted = true) => {
+    const name = file.metadata.name || file.metadata.name || '';
+    if (!name) return undefined;
+    if (quoted) return `"${name}"`;
+    return name;
+}
+
+export { cf, confirm, message, input, fatal, getName };
