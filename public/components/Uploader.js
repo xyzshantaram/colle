ba9b71615fe3f50edc8f759fc0cea906d5a9725a
Siddharth Singh <me@shantaram.xyz>
2025-04-18 08:34:40 +0530

    complete uploader cf4 refactor

--- public/components/Uploader.js
+++ public/components/Uploader.js
@@ -1,4 +1,5 @@
 import { cf, fatal } from "../deps.js";
+import { Field } from "./Field.js";
 
 function blobToBase64(blob) {
     return new Promise((resolve, _) => {
@@ -8,66 +9,115 @@ function blobToBase64(blob) {
     });
 }
 
-export const Uploader = (client, open = false) => {
-    const fileData = new cf.Store({});
+const TabButton = (name) =>
+    cf.html`<button type=button class=tab-button data-tabname="${name}">${name}</button>`;
 
-    const [elt, textarea, textfileName, uploadText, previewImg, uploadImage, fileInput, imgDescription] =
-        cf.nu("details#uploader.tabs.hidden", {
-            raw: true,
-            gimme: [
-                'textarea',
-                '#upload-filename',
-                '.upload-text',
-                '.upload-img-preview',
-                '.upload-img',
-                'input[type=file]',
-                '.upload-description'
-            ],
-            c: cf.html`
-        <summary><h2>Upload a file<h2></summary>
+const Tabs = (...names) => `<div class=tab-buttons>${names.map(TabButton).join('\n')}</div>`;
 
-        <div class=tab-buttons>
-            <button type=button class='tab-button' data-tabname="Text">Text</button>
-            <button type=button class='tab-button' data-tabname="Image">Image</button>
-        </div>
+const ImageUpload = (onChange, data) => {
+    const [elt, input, img] = cf.nu('div.upload-img-wrapper')
+        .gimme('input', 'img')
+        .html`<input type=file accept="image/png, image/jpeg, image/webp">
+           <img class=upload-img-preview>`
+        .done();
 
-        <div class=tab-content data-tabname="Text">
-            <div class=text-upload-area>
-                <div class=upload-controls>
-                    <div class="form-group">
-                        <label for=upload-filename>Filename (optional)</label>
-                        <input id=upload-filename type=text name=upload-filename placeholder="None">
+    data.on('update', ({ value }) => value.isImg && (img.src = value.contents));
+    input.onchange = async _ => await onChange(input.files[0]);
+
+    return [elt];
+}
+
+const handleUpload = async (client, fileData) => {
+    const { filename, type, description, contents, isImg } = fileData.current();
+    console.log(fileData.current())
+    const language = (!isImg) && filename && filename.includes('.')
+        ? filename.split('.').at(-1) : undefined;
+
+    fatal("Uploading, you will be redirected...", "Info");
+
+    const uuid = await client.upload(contents, type, {
+        name: !isImg ? name : undefined,
+        language,
+        description
+    });
+
+    globalThis.location = client.makeUrl(`/?view=${uuid}`);
+}
+
+const SubmitButton = (name, client, fileData) => Field({
+    type: 'submit',
+    elt: cf.nu('button')
+        .on('click', () => handleUpload(client, fileData))
+        .attr('type', 'button')
+        .cls(`upload-${name}`)
+        .content('Upload')
+        .ref()
+})[0]
+
+export const Uploader = (client, username) => {
+    const fileData = cf.store({ value: {} });
+
+    const [uploadName, filenameInput] = Field({ name: 'upload-filename', label: "Filename (optional)" });
+    console.log(uploadName, filenameInput);
+    const textSubmit = SubmitButton('text', client, fileData);
+    const imgSubmit = SubmitButton('img', client, fileData);
+
+    const handleFileChange = async (file) => {
+        if (file && ["image/png", "image/jpeg", "image/webp"].includes(file.type)) {
+            fileData.update({
+                contents: await blobToBase64(file),
+                type: file.type,
+                description: uploadDesc.value.trim(),
+                isImg: true
+            });
+            elt.querySelector('.tab-button[data-tabname="Image"]').click();
+        }
+    }
+
+    const [uploadDesc] = Field({ name: 'upload-description', label: "Image description (optional)" });
+    const [image] = ImageUpload(handleFileChange, fileData);
+
+    const [elt, textarea] =
+        cf.nu('details#uploader.tabs')
+            .children({
+                textSubmit,
+                imgSubmit,
+                filenameInput,
+                uploadDesc,
+                uploadName,
+                image
+            })
+            .attr('data-tabset-name', 'Upload')
+            .deps({ username })
+            .gimme('textarea')
+            .render(({ username }, { b }) => b.cls('hidden', !username)
+                .html`<summary><h2>Upload a file<h2></summary>
+                    ${cf.r(Tabs("Text", "Image"))}
+                    <div class=tab-content data-tabname="Text">
+                        <div class=text-upload-area>
+                            <div class=upload-controls>
+                                <cf-slot name='uploadName'></cf-slot>
+                            </div>
+                            <textarea></textarea>
+                            <cf-slot name='textSubmit'></cf-slot>
+                        </div>
                     </div>
-                </div>
-                <textarea></textarea>
-                <div class="form-group submit-group">
-                    <button type=button class=upload-text>Upload</button>
-                </div>
-            </div>
-        </div>
-        <div class=tab-content data-tabname="Image">
-            <div class="form-group">
-                <label for=upload-description>Image description (optional)</label>
-                <input class=upload-description type=text name=upload-description placeholder="None">
-            </div>
-            <div class=upload-img-wrapper>
-                <input type=file accept="image/png, image/jpeg, image/webp">
-                <img class=upload-img-preview>
-            </div>
-            <div class="form-group submit-group">
-                <button type=button class=upload-img>Upload</button>
-            </div>
-        </div>
-        `,
-            a: { 'data-tabset-name': 'Upload' }
-        });
+
+                    <div class=tab-content data-tabname="Image">
+                        <cf-slot name='uploadDesc'></cf-slot>
+                        <cf-slot name="image"></cf-slot>
+                        <cf-slot name='imgSubmit'></cf-slot>
+                    </div>
+                `)
+            .done();
 
     elt.toggleAttribute('open', open);
 
-    textarea.oninput = () => {
+    textarea.onchange = () => {
         fileData.update({
             contents: textarea.value,
-            type: 'text/plain'
+            type: 'text/plain',
+            filename: filenameInput.value.trim()
         })
     }
 
@@ -77,40 +127,7 @@ export const Uploader = (client, open = false) => {
         }
     })
 
-    const handleFile = async (file) => {
-        if (file && ["image/png", "image/jpeg", "image/webp"].includes(file.type)) {
-            fileData.update({
-                contents: await blobToBase64(file),
-                type: file.type,
-                isImg: true
-            });
-            elt.querySelector('.tab-button[data-tabname="Image"]').click();
-        }
-    }
+    globalThis.addEventListener('paste', async e => await handleFileChange(e.clipboardData.files[0]));
 
-    globalThis.addEventListener('paste', async e => await handleFile(e.clipboardData.files[0]));
-    fileInput.onchange = async _ => await handleFile(fileInput.files[0]);
-
-    uploadImage.onclick = uploadText.onclick = async () => {
-        const language = (_ => {
-            if (fileData.value.isImg) return;
-            const trimmedName = textfileName.value.trim();
-            if (!trimmedName) return;
-            const split = trimmedName.split('.');
-            if (split.at(-1) === trimmedName) return;
-            return split.at(-1);
-        })();
-
-        fatal('Uploading, you will be redirected...', 'Info');
-
-        const uuid = await client.upload(fileData.value.contents, fileData.value.type, {
-            name: fileData.value.isImg ? undefined : (textfileName.value.trim() || undefined),
-            language,
-            description: imgDescription.value || undefined
-        });
-
-
-        globalThis.location = client.makeUrl(`/?view=${uuid}`);
-    }
     return [elt];
 }
