26c860dcc2125722649bc7f88fe4280462ef8709
Siddharth Singh <me@shantaram.xyz>
2025-04-18 06:36:46 +0530

    field component done

---
+++ public/components/Field.js
@@ -0,0 +1,19 @@
+import { cf } from "../deps.js";
+
+const inputId = cf.ids('colle-input');
+
+export const Field = ({ name, type = 'text', label, classes = [], value, elt }) => {
+    const id = inputId();
+    const labelHtml = label ? cf.html`<label for="${id}">${label}</label>` : "";
+    const inputHtml = cf.html`<input type="${type}" id="${id}" name="${name}" ${value ? `value=${value}` : ''}>`;
+
+    const builder = cf.nu('div.form-group');
+    for (const c of classes) builder.cls(c);
+    if (type === 'submit') builder.cls('submit-group');
+
+    if (!elt) return builder.html(labelHtml + inputHtml).ref();
+
+    const built = builder.ref();
+    cf.insert(elt, { into: built });
+    return built;
+}
