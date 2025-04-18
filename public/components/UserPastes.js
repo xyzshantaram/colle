4ec1594e2caa4f19bcbf22e4fd6dd6c01aae0317
Siddharth Singh <me@shantaram.xyz>
2025-04-18 06:10:00 +0530

    rewrite UserPastes with cf 4

--- public/components/UserPastes.js
+++ public/components/UserPastes.js
@@ -3,17 +3,14 @@ import { cf, getName, confirm } from "../deps.js";
 const PasteItem = (item, client) => {
     const name = getName(item);
     console.log(name);
-    const [elt, del] = cf.nu('li.paste-item', {
-        raw: true,
-        gimme: ['.delete-item'],
-        c: cf.html`
-        <div class=item-name>${name}</div>
+    const [elt, del] = cf.nu('li.paste-item')
+        .gimme('.delete-item')
+        .html`<div class=item-name>${name}</div>
         <ul>
             <li><a class=view-item target="_blank" href="${client.makeUrl("/?view=" + item.uuid)}">View</a></li>
             <li><a class=delete-item href="javascript:void(0)">Delete</a></li>
-        </ul>
-        `
-    })
+        </ul>`
+        .done();
 
     del.onclick = async () => {
         if (await confirm('Are you sure you want to delete this paste?'))
@@ -25,15 +22,13 @@ const PasteItem = (item, client) => {
 
 export const UserPastes = async (client) => {
     const pastes = await client.listFiles() || [];
-    const [elt, list] = cf.nu('details#user-pastes', {
-        raw: true,
-        gimme: ['ul'],
-        contents: cf.html`
+    const [elt, list] = cf.nu('details#user-pastes')
+        .gimme('ul')
+        .html`
         <summary><h2>Your pastes</h2></summary>
         <ul class=user-paste-list></ul>`
-    });
+        .done();
 
     list.append(...pastes.map(paste => PasteItem(paste, client)));
-
     return [elt];
 }
